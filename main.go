1cc24c8db159d59e9c22e0ba619a2663fd3b4cdb
Bjorn Stange <bjorn248@gmail.com>
2023-04-09 18:35:18 -0400

    issue 2: saving progress parsing common js

--- main.go
+++ main.go
@@ -7,46 +7,48 @@ import (
 	"os"
 	"regexp"
 	"strings"
+	"text/template"
 )
 
 // Chapter defines the regex expressions to search for that denote the start and end of a
 // chapter (e.g. Street, Street Touring) of the rulebook.
 type Chapter struct {
-	name        string
-	number      string
-	subChapters []SubChapter
-	reader      *io.SectionReader
-	start       *regexp.Regexp
-	end         *regexp.Regexp
+	Name         string
+	Number       string
+	SubChapters  []SubChapter
+	Reader       *io.SectionReader
+	start        *regexp.Regexp
+	end          *regexp.Regexp
+	templateFile string
 }
 
 // SubChapter holds the name, number, and body of a subchapter of the rules (e.g. 13.2 Bodywork)
 type SubChapter struct {
-	name   string
-	number string
-	reader *io.SectionReader
+	Name   string
+	Number string
+	Reader *io.SectionReader
 }
 
 // getSubChapters returns an array of sub chapters (e.g. 13.1, 13.2) that exist for a given
 // chapter
 func getSubChapters(rules, chapterNumber string) []SubChapter {
-	subChapters := []SubChapter{}
+	SubChapters := []SubChapter{}
 	regexString := chapterNumber + `\.([0-9]+[.A-Z]*) ([^\.\n]*)\.+[\. ]([0-9]+)`
 	tableOfContents := regexp.MustCompile(regexString)
 	match := tableOfContents.FindAllStringSubmatch(rules, -1)
 	// This means there probably aren't any subchapters
 	if len(match) < 2 {
-		return subChapters
+		return SubChapters
 	}
 	for i := range match {
-		subChapters = append(subChapters,
+		SubChapters = append(SubChapters,
 			SubChapter{
-				number: fmt.Sprintf("%s.%s", chapterNumber, match[i][1]),
-				name:   match[i][2],
+				Number: fmt.Sprintf("%s.%s", chapterNumber, match[i][1]),
+				Name:   match[i][2],
 			},
 		)
 	}
-	return subChapters
+	return SubChapters
 }
 
 // readFile returns a Reader of a specific file
@@ -77,16 +79,16 @@ func readFile() *strings.Reader {
 // findSubChapterBody populates the reader field of each subchapter with the body
 // of that subchapter
 func findSubChapterBody(chapter Chapter, chapterText []byte) []SubChapter {
-	subChapters := chapter.subChapters
+	SubChapters := chapter.SubChapters
 	reader := strings.NewReader(string(chapterText))
-	for i, subChapter := range subChapters {
+	for i, subChapter := range SubChapters {
 		reader.Seek(0, 0)
 		var length int
 		seekToEnd := false
-		if i == len(subChapters)-1 {
+		if i == len(SubChapters)-1 {
 			seekToEnd = true
 		}
-		startRegexString := `(?i)` + regexp.QuoteMeta(subChapter.number) + ` ` + regexp.QuoteMeta(subChapter.name)
+		startRegexString := `(?i)` + regexp.QuoteMeta(subChapter.Number) + ` ` + regexp.QuoteMeta(subChapter.Name)
 		startRegex := regexp.MustCompile(startRegexString)
 		startMatch := startRegex.FindReaderIndex(reader)
 		reader.Seek(0, 0)
@@ -94,7 +96,7 @@ func findSubChapterBody(chapter Chapter, chapterText []byte) []SubChapter {
 			if seekToEnd == true {
 				length = reader.Len() // jump to end of reader, this is the last element
 			} else {
-				endRegexString := `(?i)` + regexp.QuoteMeta(subChapters[i+1].number) + ` ` + regexp.QuoteMeta(subChapters[i+1].name)
+				endRegexString := `(?i)` + regexp.QuoteMeta(SubChapters[i+1].Number) + ` ` + regexp.QuoteMeta(SubChapters[i+1].Name)
 				endRegex := regexp.MustCompile(endRegexString)
 				endMatch := endRegex.FindReaderIndex(reader)
 				reader.Seek(0, 0)
@@ -103,16 +105,17 @@ func findSubChapterBody(chapter Chapter, chapterText []byte) []SubChapter {
 				}
 			}
 			sectionReader := io.NewSectionReader(reader, int64(startMatch[0]), int64(length))
-			subChapters[i].reader = sectionReader
+			SubChapters[i].Reader = sectionReader
+			// uncomment to print for troubleshooting
 			subchapter, err := io.ReadAll(sectionReader)
 			if err != nil {
 				log.Fatal(err)
 			}
-			fmt.Println(subChapter.number + " " + subChapter.name)
+			fmt.Println(subChapter.Number + " " + subChapter.Name)
 			fmt.Println(string(subchapter))
 		}
 	}
-	return subChapters
+	return SubChapters
 }
 
 func getChapterReader(rules *strings.Reader, chapter Chapter) *io.SectionReader {
@@ -123,6 +126,17 @@ func getChapterReader(rules *strings.Reader, chapter Chapter) *io.SectionReader 
 	rules.Seek(0, 0)
 	length := endMatch[0] - startMatch[0]
 	return io.NewSectionReader(rules, int64(startMatch[0]), int64(length))
+}
+
+func ToVariableName(in string) string {
+	var result string
+	result = strings.Split(in, " ")[0]
+	result = regexp.MustCompile(`[^a-zA-Z0-9 ]+`).ReplaceAllString(result, "")
+	return result
+}
+
+func stringEqual(a, b string) bool {
+	return a == b
 }
 
 func main() {
@@ -136,69 +150,76 @@ func main() {
 	rules.Seek(0, 0)
 	allChapters := []Chapter{
 		{
-			name:   "Street",
-			number: "13",
-			start:  regexp.MustCompile(`\n13\. STREET CATEGORY\n`),
-			end:    regexp.MustCompile(`\n14\. STREET TOURING® CATEGORY\n`),
+			Name:         "Street",
+			Number:       "13",
+			start:        regexp.MustCompile(`\n13\. STREET CATEGORY\n`),
+			end:          regexp.MustCompile(`\n14\. STREET TOURING® CATEGORY\n`),
+			templateFile: "./templates/a/s.html.tmpl",
 		},
 		{
-			name:   "Street Touring",
-			number: "14",
+			Name:   "Street Touring",
+			Number: "14",
 			start:  regexp.MustCompile(`\n14\. STREET TOURING® CATEGORY\n`),
 			end:    regexp.MustCompile(`\n15\. STREET PREPARED CATEGORY\n`),
 		},
 		{
-			name:   "Street Prepared",
-			number: "15",
+			Name:   "Street Prepared",
+			Number: "15",
 			start:  regexp.MustCompile(`\n15\. STREET PREPARED CATEGORY\n`),
 			end:    regexp.MustCompile(`\n16\. STREET MODIFIED CATEGORY\n`),
 		},
 		{
-			name:   "Street Modified",
-			number: "16",
+			Name:   "Street Modified",
+			Number: "16",
 			start:  regexp.MustCompile(`\n16\. STREET MODIFIED CATEGORY\n`),
 			end:    regexp.MustCompile(`\n17\. PREPARED CATEGORY\n`),
 		},
 		{
-			name:   "Prepared",
-			number: "17",
+			Name:   "Prepared",
+			Number: "17",
 			start:  regexp.MustCompile(`\n17\. PREPARED CATEGORY\n`),
 			end:    regexp.MustCompile(`\n18\. MODIFIED CATEGORY\n`),
 		},
 		{
-			name:   "Modified",
-			number: "18",
+			Name:   "Modified",
+			Number: "18",
 			start:  regexp.MustCompile(`\n18\. MODIFIED CATEGORY\n`),
 			end:    regexp.MustCompile(`\n19\. KART CATEGORY\n`),
 		},
 		{
-			name:   "Solo Spec Coupe",
-			number: "20",
+			Name:   "Solo Spec Coupe",
+			Number: "20",
 			start:  regexp.MustCompile(`\n20\. SOLO® SPEC COUPE \(SSC\)\n`),
 			end:    regexp.MustCompile(`\n21\. PROSOLO® NATIONAL SERIES RULES\n`),
 		},
 		{
-			name:   "Xtreme Street",
-			number: "n/a",
+			Name:   "Xtreme Street",
+			Number: "n/a",
 			start:  regexp.MustCompile(`\nClassic American Muscle \/ Xtreme Street Category\n`),
 			end:    regexp.MustCompile(`\nElectrical Vehicle Experimental \(EVX\)\n`),
 		},
 		{
-			name:   "EVX",
-			number: "n/a",
+			Name:   "EVX",
+			Number: "n/a",
 			start:  regexp.MustCompile(`\nElectrical Vehicle Experimental \(EVX\)\n`),
 			end:    regexp.MustCompile(`\nAPPENDIX C - SOLO® ROLL BAR STANDARDS\n`),
 		},
 	}
 
+	funcMap := template.FuncMap{
+		"readAll":     io.ReadAll,
+		"varName":     ToVariableName,
+		"stringEqual": stringEqual,
+	}
+
 	for i := range allChapters {
-		if allChapters[i].number != "n/a" {
-			subChapters := getSubChapters(string(rulesBytes), allChapters[i].number)
-			allChapters[i].subChapters = subChapters
+		if allChapters[i].Number != "n/a" {
+			SubChapters := getSubChapters(string(rulesBytes), allChapters[i].Number)
+			allChapters[i].SubChapters = SubChapters
 		}
-		fmt.Printf("Currently processing: %s\n", allChapters[i].name)
+		fmt.Printf("Currently processing: %s\n", allChapters[i].Name)
 		chapterReader := getChapterReader(rules, allChapters[i])
-		allChapters[i].reader = chapterReader
+		allChapters[i].Reader = chapterReader
 
 		chapterText, err := io.ReadAll(chapterReader)
 		if err != nil {
@@ -207,8 +228,8 @@ func main() {
 		}
 
 		// remove all form feed (i.e. ) chapter title lines
-		if allChapters[i].number != "n/a" {
-			remove := regexp.MustCompile(`\n\f` + allChapters[i].number + `\. .+\n`)
+		if allChapters[i].Number != "n/a" {
+			remove := regexp.MustCompile(`\n\f` + allChapters[i].Number + `\. .+\n`)
 			chapterText = remove.ReplaceAll(chapterText, []byte{})
 		}
 
@@ -216,9 +237,52 @@ func main() {
 		remove := regexp.MustCompile(`(?i)([0-9]+ — )*\d{4} SCCA® NATIONAL SOLO® RULES( )*(— [0-9]+)*`)
 		chapterText = remove.ReplaceAll(chapterText, []byte{})
 
-		if allChapters[i].number != "n/a" && len(allChapters[i].subChapters) > 0 {
-			allChapters[i].subChapters = findSubChapterBody(allChapters[i], chapterText)
+		if allChapters[i].Number != "n/a" && len(allChapters[i].SubChapters) > 0 {
+			allChapters[i].SubChapters = findSubChapterBody(allChapters[i], chapterText)
 		}
+
+		/* TODO actually parse individual HTML template
+		if allChapters[i].templateFile != "" {
+			fmt.Println("Generating class specific page...")
+			commonJS := template.New("common.js.tmpl").Funcs(funcMap)
+			tpl, err := commonJS.ParseFiles("templates/common.js.tmpl")
+			if err != nil {
+				log.Fatal("Could not parse template", err)
+			}
+
+			outFile, err := os.Create("src/common.js")
+			if err != nil {
+				log.Fatal("Could not create file", err)
+			}
+
+			err = tpl.Execute(outFile, allChapters[i])
+			if err != nil {
+				log.Fatal("Could not execute template", err)
+			}
+
+			outFile.Close()
+		}
+		*/
 	}
+
 	fmt.Printf("%+v", allChapters)
+
+	fmt.Println("Generating common.js...")
+	commonJS := template.New("common.js.tmpl").Funcs(funcMap)
+	tpl, err := commonJS.ParseFiles("templates/common.js.tmpl")
+	if err != nil {
+		log.Fatal("Could not parse template", err)
+	}
+
+	outFile, err := os.Create("src/common.js")
+	if err != nil {
+		log.Fatal("Could not create file", err)
+	}
+
+	err = tpl.Execute(outFile, allChapters)
+	if err != nil {
+		log.Fatal("Could not execute template", err)
+	}
+
+	outFile.Close()
 }
